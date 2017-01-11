7fa12169d40f973e143471765874737e9dd462ad
Theeeus <matheushsalves@gmail.com>
2017-01-11 16:09:59 -0200

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var browserBookmarks = require('browser-bookmarks');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req,res) {
+	browserBookmarks.getChrome().then(function(bookmarks){
+	var n = bookmarks.length;
+	var random = Math.floor(Math.random() * (n - 0 + 1)) + 0;
+	var url = bookmarks[random].url;
+	res.redirect(url);
+});
+});
+
+app.listen(port, function() {
+	console.log('Shuffle App listening on port' + port);
+});
